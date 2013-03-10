de0298a013531bd561b38453bae6234d1ff7516c
Simon Piette <simon.piette@savoirfairelinux.com>
2013-03-10 17:09:21 -0400

    more extensive rspec testing

--- spec/classes/ntp_init_spec.rb
+++ spec/classes/ntp_init_spec.rb
@@ -3,48 +3,49 @@ require 'spec_helper'
 describe 'ntp' do
   let(:title) { 'ntp' }
 
-  let(:facts) {
-    { :osfamily => 'RedHat' }
-  }
-
-  describe 'ntp class without any parameters' do 
-    let(:params) {{ }}
-
-    it { should create_class('ntp') }
-    it { should create_file('/etc/ntp.conf') }
-    it {
-      should create_file('/etc/ntp.conf')\
-      .with_content(/^server pool.ntp.org$/)
-    }
-  end
-
-  describe 'ntp class with service parameter' do 
-    let(:params) {
-      { :servers => [
-        "0.debian.pool.ntp.org",
-        "1.debian.pool.ntp.org",
-        "time.nrc.ca",
-        ]
-      }
-    }
-
-    it { should create_class('ntp') }
-    it { should create_file('/etc/ntp.conf') }
-    it {
-      should create_file('/etc/ntp.conf')\
-      .with_content(/^server 0.debian.pool.ntp.org$/)\
-      .with_content(/^server 1.debian.pool.ntp.org$/)\
-      .with_content(/^server time.nrc.ca$/)
-    }
-    it {
-      should create_service('ntpd')
-    }
-  end
   ['Debian', 'RedHat'].each do |osfamily|
-    if osfamily == 'RedHat' 
-      it { should create_service('ntpd') }
-    else
-      it { should create_service('ntp') }
+    describe "ntp class without any parameters on #{osfamily}" do 
+      let(:params) {{ }}
+      let(:facts) { { :osfamily => osfamily } }
+
+      it { should create_class('ntp') }
+      it { should create_package('ntp') }
+      it { should create_file('/etc/ntp.conf') }
+      it {
+        should create_file('/etc/ntp.conf')\
+        .with_content(/^server pool.ntp.org$/)
+      }
+      if osfamily == 'RedHat' 
+        it { should create_service('ntpd') }
+      else
+        it { should create_service('ntp') }
+      end
+    end
+    describe "ntp class with service parameter on #{osfamily}" do 
+      let(:facts) { { :osfamily => osfamily } }
+      let(:params) {
+        { :servers => [
+          "0.debian.pool.ntp.org",
+          "1.debian.pool.ntp.org",
+          "time.nrc.ca",
+          ]
+        }
+      }
+
+      it { should create_class('ntp') }
+      it { should create_package('ntp') }
+      it { should create_file('/etc/ntp.conf') }
+      it {
+        should create_file('/etc/ntp.conf')\
+        .with_content(/^server 0.debian.pool.ntp.org$/)\
+        .with_content(/^server 1.debian.pool.ntp.org$/)\
+        .with_content(/^server time.nrc.ca$/)
+      }
+      if osfamily == 'RedHat' 
+        it { should create_service('ntpd') }
+      else
+        it { should create_service('ntp') }
+      end
     end
   end
 end
